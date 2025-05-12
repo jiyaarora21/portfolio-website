ab7f485cc976bf3f5d3c0410839a187b00bcc4f3
jiyaarora21 <arorajiya2101@gmail.com>
2025-05-12 12:28:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,3 +52,14 @@ app.post('/contact', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
